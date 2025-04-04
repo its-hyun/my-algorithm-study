754ded2953e7b1592209ab7fa80a821b74c4ee4e
Hyun <itsmine096@gmail.com>
2025-04-04 23:36:24 +0900

    LeetCode - Two Sum

--- src/main/java/LeetCode/Two_Sum/BruteForce.java
+++ src/main/java/LeetCode/Two_Sum/BruteForce.java
@@ -3,20 +3,13 @@ package LeetCode.Two_Sum;
 public class BruteForce {
 
 	public int[] twoSum(int[] nums, int target) {
-		int[] ans = new int[2];
-		for (int i = 0; i < nums.length; i++) {
-			for (int j = 0; j < nums.length; j++) {
-				if (i == j) {
-					continue;
-				}
-
+		for (int i = 0; i < nums.length - 1; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
 				if (nums[i] + nums[j] == target) {
-					ans[0] = i;
-					ans[1] = j;
-					return ans;
+					return new int[] {i, j};
 				}
 			}
 		}
-		return ans;
+		return new int[2];
 	}
 }
